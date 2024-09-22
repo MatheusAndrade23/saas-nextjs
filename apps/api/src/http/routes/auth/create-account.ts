b4e3c1ef2d187c5a3eddd5c6a89166f8c0b41aad
Matheus Andrade <matheusandrade.ma2003@gmail.com>
2024-09-22 15:33:19 -0300

    Create Account Route

--- apps/api/src/http/routes/auth/create-account.ts
+++ apps/api/src/http/routes/auth/create-account.ts
@@ -1,21 +1,54 @@
 import type { FastifyInstance } from 'fastify'
-import type { ZodTypeProvider } from 'fastify-type-provider-zod'
 import { z } from 'zod'
 
+import { hash } from 'bcryptjs'
+
+import { prisma } from '@/lib/prisma'
+
+const createUserBodySchema = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  password: z.string().min(6),
+})
+
+type CreateUserBody = z.infer<typeof createUserBodySchema>
+
 export async function createAccount(app: FastifyInstance) {
-  app.withTypeProvider<ZodTypeProvider>().post(
+  app.post<{ Body: CreateUserBody }>(
     '/users',
     {
       schema: {
-        body: z.object({
-          name: z.string(),
-          email: z.string().email(),
-          password: z.string().min(6),
-        }),
+        body: createUserBodySchema,
       },
     },
-    () => {
-      return 'User created!'
+    async (request, reply) => {
+      const { name, email, password } = request.body
+
+      const userWithSameEmail = await prisma.user.findUnique({
+        where: {
+          email,
+        },
+      })
+
+      if (userWithSameEmail) {
+        return reply.status(400).send({
+          message: 'User with same email already exists',
+        })
+      }
+
+      const passwordHash = await hash(password, 6)
+
+      prisma.user.create({
+        data: {
+          name,
+          email,
+          passwordHash,
+        },
+      })
+
+      return reply.status(201).send({
+        message: 'User created',
+      })
     }
   )
 }
